6b11d395440a38577d56f43dfad3bdc7bd333287
jeongsooyoung <code.ameba@gmail.com>
2020-08-16 16:47:29 +0900

    MODIFY: add quote icons

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -1,18 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 
 function Quotes({ randomQuote }) {
   return (
     <QuotesWrapper>
-      <h1>{randomQuote.text}</h1>
-      <h3>{randomQuote.author !== null ? randomQuote.author : 'unknown'}</h3>
+      <section className='textWrapper'>
+        <FaQuoteLeft className='leftQuote' />
+        <h1>{randomQuote.text}</h1>
+        <FaQuoteRight className='rightQuote' />
+      </section>
+      <h3>
+        - {randomQuote.author !== null ? randomQuote.author : 'unknown'} -
+      </h3>
     </QuotesWrapper>
   );
 }
 
 const QuotesWrapper = styled.article`
   width: 100%;
-  height: 100%;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .textWrapper {
+    margin: 20px 0;
+    text-align: center;
+    position: relative;
+
+    .leftQuote {
+      font-size: 35px;
+      position: absolute;
+      top: -50px;
+      left: 0;
+    }
+
+    .rightQuote {
+      font-size: 35px;
+      position: absolute;
+      bottom: -50px;
+      right: 0;
+    }
+
+    h1 {
+      font-size: 30px;
+      line-height: 45px;
+      font-weight: 600;
+    }
+  }
+
+  h3 {
+    font-size: 18px;
+    letter-spacing: 1px;
+    margin-top: 50px;
+  }
 `;
 
 export default Quotes;
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -19,8 +19,6 @@ function Main() {
       .then((res) => setRandomQuote(res.data[idx]));
   };
 
-  console.log(randomQuote);
-
   return (
     <MainWrapper>
       <Quotes randomQuote={randomQuote} />
@@ -33,13 +31,12 @@ const GlobalStyles = createGlobalStyle`
   body {
     width: 100%;
     height: 100%;
-    background-color: #fff;
   }
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: Helvetica, sans-serif;
+    font-family: serif;
     font-size: 16px;
     font-weight: 400;
     line-height: 16px;
@@ -52,7 +49,11 @@ const GlobalStyles = createGlobalStyle`
 
 const MainWrapper = styled.main`
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ivory;
 `;
 
 export default Main;
